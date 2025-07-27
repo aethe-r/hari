f62aa03a088c471b3d6f06b3814cd43fff1c881a
Joshua Astron <hajickio@gmail.com>
2025-07-27 20:44:34 +1000

    Updated the code base to be cross-compatible with all systems.

--- src/mysketch/MainSketch.java
+++ src/mysketch/MainSketch.java
@@ -1,13 +1,17 @@
 package mysketch;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
 import processing.event.MouseEvent;
 
 public class MainSketch extends PApplet {
 
+    // Canvas & drawing
+    PGraphics canvas;
+
     // Colors & brush
-    int bgColor     = color(240);
-    int brushColor  = color(0);
+    int bgColor = color(240);
+    int brushColor = color(0);
     float brushSize = 12;
 
     boolean showHUD = true;
@@ -17,60 +21,101 @@ public class MainSketch extends PApplet {
     }
 
     public void setup() {
-        background(bgColor);
+        canvas = createGraphics(width, height);
+        clearCanvas();
         smooth();
         strokeCap(ROUND);
         noFill();
     }
 
     public void draw() {
-        // Nothing continuous needed; drawing happens in mouseDragged().
-        if (showHUD) drawHUD();
-    }
+        background(bgColor);
+        image(canvas, 0, 0);
 
-    // Draw while dragging
-    public void mouseDragged() {
-        stroke(brushColor);
-        strokeWeight(brushSize);
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-
-    // Mouse wheel = brush size
-    public void mouseWheel(MouseEvent event) {
-        float e = event.getCount();         // negative = up
-        brushSize = constrain(brushSize - e, 1, 200);
-    }
-
-    // Keyboard shortcuts
-    public void keyPressed() {
-        switch (key) {
-            case '+': case '=': brushSize = min(brushSize + 2, 200); break;
-            case '-': case '_': brushSize = max(brushSize - 2, 1);   break;
-            case 'c': case 'C': background(bgColor);                 break;
-            case 's': case 'S': saveFrame("savedImages/paint-####.png");          break;
-            case 'h': case 'H': showHUD = !showHUD;                   break;
-            // Quick color picks
-            case '1': brushColor = color(0);                 break;   // black
-            case '2': brushColor = color(255);               break;   // white
-            case '3': brushColor = color(255, 0, 0);         break;   // red
-            case '4': brushColor = color(0, 255, 0);         break;   // green
-            case '5': brushColor = color(0, 0, 255);         break;   // blue
+        if (showHUD) {
+            drawHUD();
         }
     }
 
-    // Simple on-canvas heads-up display
+    public void mouseDragged() {
+        canvas.beginDraw();
+        canvas.stroke(brushColor);
+        canvas.strokeWeight(brushSize);
+        canvas.line(pmouseX, pmouseY, mouseX, mouseY);
+        canvas.endDraw();
+    }
+
+    public void mouseWheel(MouseEvent event) {
+        float e = event.getCount();
+        brushSize = constrain(brushSize - e, 1, 200);
+    }
+
+    public void keyPressed() {
+        switch (key) {
+            case '+':
+            case '=':
+                brushSize = min(brushSize + 2, 200);
+                break;
+            case '-':
+            case '_':
+                brushSize = max(brushSize - 2, 1);
+                break;
+            case 'c':
+            case 'C':
+                clearCanvas();
+                break;
+            case 's':
+            case 'S':
+                canvas.save("savedImages/paint-" + nf(frameCount, 4) + ".png");
+                break;
+            case 'h':
+            case 'H':
+                showHUD = !showHUD;
+                redraw(); // ensures screen is refreshed
+                break;
+            // Colors
+            case '1':
+                brushColor = color(0);
+                break;
+            case '2':
+                brushColor = color(255);
+                break;
+            case '3':
+                brushColor = color(255, 0, 0);
+                break;
+            case '4':
+                brushColor = color(0, 255, 0);
+                break;
+            case '5':
+                brushColor = color(0, 0, 255);
+                break;
+        }
+    }
+
+    void clearCanvas() {
+        canvas.beginDraw();
+        canvas.background(bgColor);
+        canvas.endDraw();
+    }
+
     void drawHUD() {
         pushStyle();
         fill(0, 150);
         noStroke();
-        rect(0, 0, 240, 70);
+        rect(0, 0, 250, 70);
         fill(255);
         textSize(12);
         text("Brush size: " + nf(brushSize, 1, 1) +
-             "\n[+/- or mouse wheel] size" +
-             "\n1-5 colors | C clear | S save | H hide HUD",
-             10, 18);
+                "\n[+/- or mouse wheel] size" +
+                "\n1-5 colors | C clear | S save | H toggle HUD",
+                10, 18);
         popStyle();
+    }
+
+    @Override
+    public void exit() {
+        // Add any cleanup code here if needed
+        super.exit(); // <-- This actually closes the window
     }
 
     public static void main(String[] args) {
